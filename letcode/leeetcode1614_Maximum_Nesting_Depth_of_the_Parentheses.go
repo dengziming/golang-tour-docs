00c720310f17c4b3c66d29fce4d00694f6437ae9
dengziming <dengziming1993@gmail.com>
2022-02-25 11:20:00 +0800

    Leetcode 1614. Maximum Nesting Depth of the Parentheses

---
+++ letcode/leeetcode1614_Maximum_Nesting_Depth_of_the_Parentheses.go
@@ -0,0 +1,16 @@
+package letcode
+
+func maxDepth(s string) (ans int) {
+	size := 0
+	for _, ch := range s {
+		if ch == '(' {
+			size++
+			if size > ans {
+				ans = size
+			}
+		} else if ch == ')' {
+			size--
+		}
+	}
+	return
+}
