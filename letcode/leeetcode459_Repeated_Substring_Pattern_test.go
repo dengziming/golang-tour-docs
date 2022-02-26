5a73594f3f835aa2d161e25b4b8d668be56ca853
dengziming <dengziming1993@gmail.com>
2022-02-26 22:54:35 +0800

    Leetcode 459. Repeated Substring Pattern

---
+++ letcode/leeetcode459_Repeated_Substring_Pattern_test.go
@@ -0,0 +1,13 @@
+package letcode
+
+import "testing"
+
+func TestRepeatedSubstringPattern(t *testing.T) {
+
+}
+
+func TestKmp(t *testing.T) {
+	if kmp("abcabc", "abcabdc") {
+		t.Error("测试失败")
+	}
+}
