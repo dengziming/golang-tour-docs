ea7b1c9b78e4cba7f12c944c14fbf65abdbce861
dengziming <dengziming1993@gmail.com>
2022-03-08 14:31:51 +0800

    Leetcode 56. Merge Intervals

---
+++ letcode/leetcode0056_Merge_Intervals.go
@@ -0,0 +1,28 @@
+package letcode
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	max := func(a int, b int) int {
+		if a > b {
+		return a
+	}
+		return b
+	}
+
+	var res [][]int
+	for _, interval := range intervals{
+		var length = len(res)
+		if length == 0 || res[length-1][1] < interval[0]{
+			res = append(res, interval)
+		}else{
+			res[length-1][1] = max(res[length-1][1], interval[1])
+		}
+	}
+	return res
+
+}
